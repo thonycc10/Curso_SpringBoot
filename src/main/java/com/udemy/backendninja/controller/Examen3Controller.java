5f61eabf6dea2125c4d3b871029c9c44b674a632
thony <thony10.rc.c@gmail.com>
2018-05-12 13:23:49 -0500

    13:23 - Redirect dos formas String o RedirectView.

--- src/main/java/com/udemy/backendninja/controller/Examen3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Examen3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.backendninja.model.Person;
 
@@ -16,6 +17,18 @@ public class Examen3Controller {
 
 	public final static String FORM_VIEW = "form";
 	public final static String RESULT_VIEW = "result";
+	
+	/*Forma 1 redirect*/
+//	@GetMapping("/")
+//	public String redirect() {
+//		return "redirect:/example3/showform";
+//	}
+
+	/*Forma 2*/
+	@GetMapping("/")
+	public RedirectView redirect() {
+		return new RedirectView("/example3/showform");
+	}
 	
 	@GetMapping("/showform")
 	public String showForm(Model model) {
