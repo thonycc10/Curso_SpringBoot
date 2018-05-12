558bf3ec442a18917624e09a354ce4aecff89ab1
thony <thony10.rc.c@gmail.com>
2018-05-12 11:48:17 -0500

    12/05/2018 - Insertando datos completos thy

--- src/main/java/com/udemy/backendninja/controller/exampleController.java
+++ src/main/java/com/udemy/backendninja/controller/exampleController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.backendninja.model.Person;
+
 @Controller
 @RequestMapping("/example")
 public class exampleController {
@@ -17,7 +19,7 @@ public class exampleController {
 //	@RequestMapping(value = "/exampleString", method = RequestMethod.GET)
 	@GetMapping("/exampleString")
 	public String exampleString(Model model) {
-		model.addAttribute("name", "Thonyd");
+		model.addAttribute("person", new Person("jon", 23));
 		return EXAMPLE_VIEW;
 	}
 	
@@ -26,7 +28,7 @@ public class exampleController {
 	@GetMapping("/exampleMAV")
 	public ModelAndView exampleMAV() {
 		ModelAndView andView= new ModelAndView(EXAMPLE_VIEW); // añdaes el nombre de la plantilla html
-		andView.addObject("name", "Remy");
+		andView.addObject("person",  new Person("thony", 30));
 //		return new ModelAndView(EXAMPLE_VIEW);
 		return andView;
 	}
