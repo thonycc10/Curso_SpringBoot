c4b87649b66a6b447d1d3ba731123776dc14272c
thony <thony10.rc.c@gmail.com>
2018-05-12 12:29:29 -0500

    12:30 - Recibiendo una peticion GET (forma 1)

---
+++ src/main/java/com/udemy/backendninja/controller/Example2Controller.java
@@ -0,0 +1,24 @@
+package com.udemy.backendninja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/example2")
+public class Example2Controller {
+	
+	private static final String EXAMPLE2_VIEW = "example2";
+	
+	/* Para representar paramentros OJO
+	 * localhost:8081/example2/request1?nm=jon
+	 * */
+	@GetMapping("/request1")
+	public ModelAndView request1(@RequestParam(name="nm", required=false, defaultValue = "NULL") String name) {
+		ModelAndView mav = new ModelAndView(EXAMPLE2_VIEW);
+		mav.addObject("nm_in_model", name);
+		return mav;
+	}
+}
