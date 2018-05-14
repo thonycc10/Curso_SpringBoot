d6ada7ed966a676a3a4b2853726be413c7bf4946
thony <thony10.rc.c@gmail.com>
2018-05-13 22:54:44 -0500

    10:59 - Tiempo de peticion

---
+++ src/main/java/com/udemy/backendninja/component/RequestTimeInterceptor.java
@@ -0,0 +1,38 @@
+package com.udemy.backendninja.component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+
+
+@Component("requestTimeInterceptor") // creamos el bean
+public class RequestTimeInterceptor extends HandlerInterceptorAdapter{
+	
+	// se extends HandlerInterceptorAdapter luego en sourc/override method
+	
+	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
+		
+	@Override // se ejecuta dentro del metodo
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		request.setAttribute("startTime", System.currentTimeMillis()); // tiempo en minisegundos
+		return true;
+	}
+	
+	@Override // se ejecuta justo antes que se muestre la vista return
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		long startTime = (long) request.getAttribute("startTime");
+		LOG.info("--REQUEST URL: '" + request.getRequestURL().toString() + "' -- TOTAL TIME: '" + (System.currentTimeMillis() - startTime) + "'ms" );
+	}
+
+
+	
+	
+	
+
+}
---
+++ src/main/java/com/udemy/backendninja/config/WebMvcConfiguration.java
@@ -0,0 +1,25 @@
+package com.udemy.backendninja.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.udemy.backendninja.component.RequestTimeInterceptor;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
+	// el component de time se configura aqui requestTimeInterceptor
+	
+	@Autowired
+	@Qualifier("requestTimeInterceptor")
+	private RequestTimeInterceptor requestTimeInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestTimeInterceptor);
+	}
+	
+	
+}
