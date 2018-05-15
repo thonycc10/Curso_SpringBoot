1b98d308619a32cb6341db5d38d9d7fe9e998f03
thony <thony10.rc.c@gmail.com>
2018-05-15 00:10:30 -0500

    Creando la entidad, el modelo y la tabla en base de datos

---
+++ src/main/java/com/udemy/backendninja/model/ContactModel.java
@@ -0,0 +1,71 @@
+package com.udemy.backendninja.model;
+// se usa para interactual con el controller para seguridad
+
+import javax.persistence.Column;
+
+public class ContactModel {
+
+	private int id;
+	private String firstname;
+	private String lastname;
+	private String telephone;
+	private String city;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public ContactModel(int id, String firstname, String lastname, String telephone, String city) {
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.telephone = telephone;
+		this.city = city;
+	}
+
+	public ContactModel() {
+	}
+
+	@Override
+	public String toString() {
+		return "ContactModel [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", telephone="
+				+ telephone + ", city=" + city + "]";
+	}
+
+}
