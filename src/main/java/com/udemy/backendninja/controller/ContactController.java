80e7983f8eec81ccf48b3767f0fcd279cabb53a5
thony <thony10.rc.c@gmail.com>
2018-05-15 22:42:41 -0500

    crear boton cancel th:href

--- src/main/java/com/udemy/backendninja/controller/ContactController.java
+++ src/main/java/com/udemy/backendninja/controller/ContactController.java
@@ -1,22 +1,30 @@
 package com.udemy.backendninja.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.udemy.backendninja.constant.ViewConstant;
+import com.udemy.backendninja.model.ContactModel;
 
 @Controller
 @RequestMapping("/contact")
 public class ContactController {
 
-	@GetMapping("/")
-	public String redirectContac() {
-		return "redirect:/contact/contactform";
+//	@GetMapping("/")
+//	public String redirectContac() {
+//		return "redirect:/contact/contactform";
+//	}
+	
+	@GetMapping("/cancel")
+	public String cancel() {
+		return ViewConstant.CONTACTS;
 	}
 	
 	@GetMapping("/contactform")
-	public String showContactForm() {
+	public String redirectContac(Model model) {
+		model.addAttribute("contact", new ContactModel());
 		return ViewConstant.CONTACT_FORM;
 	}
 	
