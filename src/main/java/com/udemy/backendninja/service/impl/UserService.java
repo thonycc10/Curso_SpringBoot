a36f9c449b4718dfee79ee86a2337ae819789c3f
anthony_carrasco <anthony.carrasco@attlaris.pe>
2018-05-21 09:35:56 -0500

    Corregido el problema de UserDetailService

--- src/main/java/com/udemy/backendninja/service/impl/UserService.java
+++ src/main/java/com/udemy/backendninja/service/impl/UserService.java
@@ -1,9 +1,16 @@
 package com.udemy.backendninja.service.impl;
 
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
-public class UserService {
+public class UserService implements UserDetailsService {
 
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
